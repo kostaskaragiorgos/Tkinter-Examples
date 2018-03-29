311fcb59d904001e82275b4f2f99693d87a6b218
kostaskaragiorgos <34107442+kostaskaragiorgos@users.noreply.github.com>
2018-03-29 21:31:51 +0300

    Create about shopping list gui version.py

---
+++ about shopping list gui version.py
@@ -0,0 +1,12 @@
+This file is a how to use file of the shopping list gui version.py file
+
+the shopping list gui version.py file contains 4 buttons: insert, save, clear and delete
+The insert button inserts a value to the list file 
+The save button saves a .txt file which contains the inserted list
+The clear button clears the input
+The delete button deletes the last 
+
+FUTURE UPDATE
+
+HELP MENU BAR
+
