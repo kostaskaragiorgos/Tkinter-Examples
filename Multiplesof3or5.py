935cc7ea69dc178f42f0168a7bb8ad708b4ddbb2
kostaskaragiorgos <34107442+kostaskaragiorgos@users.noreply.github.com>
2018-04-26 13:15:43 +0300

    Delete Multiplesof3or5.py

--- Multiplesof3or5.py
+++
@@ -1,9 +0,0 @@
-def sumofm3or5():
-    sum = 0
-    for i in range(1000):
-        if i%3==0 or i%5==0:
-            print("the",i,"is multiple of 3 or 5")
-            sum +=i
-    return sum
-
-print("The sum of all the multiples of 3 or 5  below 1000 is",sumofm3or5())
