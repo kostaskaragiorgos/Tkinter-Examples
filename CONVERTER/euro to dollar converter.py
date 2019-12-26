f6a4260297e87f4412aacc716a59f69d46be98e4
kostas karagiorgos <34107442+kostaskaragiorgos@users.noreply.github.com>
2019-12-26 21:28:41 +0200

    Update euro to dollar converter.py

--- CONVERTER/euro to dollar converter.py
+++ CONVERTER/euro to dollar converter.py
@@ -12,6 +12,8 @@ class GUI():
         dollarv =1.2369
         self.frame = Frame(master)
         self.frame.pack()
+        
+        
 
         self.label = Label(self.frame, text = "Insert iteam")
         self.label.grid(row=0)
@@ -39,6 +41,28 @@ class GUI():
         self.mylabel = Label(self.frameb,textvariable = self.myText,font = "Arial 20")
         self.mylabel.pack()
 
+         
+        self.menu = Menu(self.master)
+        
+        self.file_menu = Menu(self.menu,tearoff = 0)
+        self.file_menu.add_command(label = "Convert",accelerator = 'Ctrl+T',command = self.convert)
+        self.file_menu.add_command(label="Exit",accelerator= 'Alt+F4',command = self.exitmenu)
+        self.menu.add_cascade(label = "File",menu=self.file_menu)
+        
+        
+        self.about_menu = Menu(self.menu,tearoff = 0)
+        self.about_menu.add_command(label = "About",accelerator= 'Ctrl+I',command=self.aboutmenu)
+        self.menu.add_cascade(label="About",menu=self.about_menu)
+        
+        self.help_menu = Menu(self.menu,tearoff = 0)
+        self.help_menu.add_command(label = "Help",accelerator = 'Ctrl+F1',command=self.helpmenu)
+        self.menu.add_cascade(label="Help",menu=self.help_menu)
+        
+        self.master.config(menu=self.menu)
+        self.master.bind('<Alt-F4>',lambda event: self.exitmenu())
+        self.master.bind('<Control-F1>',lambda event: self.helpmenu())
+        self.master.bind('<Control-i>',lambda event:self.aboutmenu())
+        self.master.bind('<Control-t>',lambda event:self.convert())
         
 
     def convert(self):
